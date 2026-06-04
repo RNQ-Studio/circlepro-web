88350ff6c3fe2f32b1b8f0190ddecbf797fdc790
Ramadhan Rosihadi Perdana <ramadhanrp.developer@gmail.com>
2026-06-04 14:10:21 +0700

    diagnostic: add temporary diagnostic user endpoint

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,16 @@ Route::get('/', function () {
 
 Route::get('/articles', [PublicArticleController::class, 'index'])->name('public.articles.index');
 Route::get('/articles/{slug}', [PublicArticleController::class, 'show'])->name('public.articles.show');
+
+Route::get('/diagnostic-user', function () {
+    $user = \App\Models\User::withTrashed()->find(3);
+    if (!$user) {
+        return response()->json(['error' => 'User 3 not found']);
+    }
+    
+    return response()->json([
+        'user' => $user->toArray(),
+        'exists' => \App\Models\User::find(3) !== null,
+        'deleted' => $user->trashed(),
+    ]);
+});
